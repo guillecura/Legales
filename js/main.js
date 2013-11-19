e892cf25df47cbda19da3012d79cb3702810fd11
Guille Cura <cura.gf@gmail.com>
2013-11-19 19:00:03 -0200

    build failed, main.js issue

--- js/main.js
+++ js/main.js
@@ -4,27 +4,8 @@
 		bgImgs;
 
 	$(document).on("scroll", function (event) {
-		var scrollTop = $(document).scrollTop(),
-			scrollMod = scrollTop / ($(document).height() - 100) * 13;
+		var scrollTop = $(document).scrollTop();
 
 		$('#home').css('background-position-y', scrollTop / 2);
-	});
-
-
-	$("a[href*=#]").on("click", function (e) {
-		if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-			var targetOffset, 
-				$target = $(this.hash);
-			$target = $target.length && $target || $("[name=" + this.hash.slice(1) + "]");
-			
-			if ($target.length) {
-				targetOffset = $target.offset().top;
-				$("html, body").animate({
-					scrollTop: targetOffset
-				}, 400);
-
-				return false;
-			}
-		}
 	});
 }(jQuery));
