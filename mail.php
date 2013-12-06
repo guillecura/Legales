91085cf7cfc1b2b2bf4c8b5ed39f3fa3743640ad
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-12-06 18:37:00 -0200

    Mail with "from" field

--- mail.php
+++ mail.php
@@ -1,23 +1,24 @@
-<html>
-<body style="background:#FFF;">
 <?php
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$empresa = $_POST['empresa'];
-$apellido = $_POST['apellido'];
-$telefono = $_POST['telefono'];
-$mensaje = "Este mensaje fue enviado por " . $nombre . " " . $apellido . " \r\n";
-$mensaje .= "De la empresa " . $empresa . " \r\n";
-$mensaje .= "Su teléfono es: " . $telefono . " \r\n";
-$mensaje .= "Su e-mail es: " . $email . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+	$email    = (isset($_POST["email"])) ? $_POST["email"] : "email_desconocido";
+	$nombre   = (isset($_POST["nombre"])) ? $_POST["nombre"] : "Anónimo";
+	$apellido = (isset($_POST["apellido"])) ? $_POST["apellido"] : "";
+	$empresa  = (isset($_POST["empresa"])) ? $_POST["empresa"] : "";
+	$telefono = (isset($_POST["telefono"])) ? $_POST["telefono"] : "";
 
-$para = 'mail de martha';
-$asunto = 'Plaza Legal | Consulta Web';
+	$mensaje  = "Este mensaje fue enviado por " . $nombre." ".$apellido." \r\n";
+	if ($empresa != "")
+		$mensaje .= "De la empresa ".$empresa." \r\n";
+	if ($telefono != "")
+		$mensaje .= "Su teléfono es: ".$telefono." \r\n";
+	$mensaje .= "Su e-mail es: ".$email." \r\n";
+	$mensaje .= "Mensaje: ".$_POST["mensaje"]." \r\n";
+	$mensaje .= "Enviado el ".date("d/m/Y", time());
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
-header('Location: ../index.html');
+	$para   = "consulta@plazalegal.com.uy";
+	$asunto = "Plaza Legal | Consulta Web";
+
+	$headers = ($email != "email_desconocido") ? "From: ".$email : "";
+
+	mail($para, $asunto, utf8_decode($mensaje), $headers);
+	header("Location: ../index.html");
 ?>
-</body>
-</html>
